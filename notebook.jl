3e7429e444a3647f847086f7ca3a4ec8a42395d4
david.mccabe <david.mccabe@dysismedical.com>
2025-08-18 23:34:54 +0100

    hello world

---
+++ notebook.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.20.16
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c4779372-7c81-11f0-043a-897064d0eec1
+println("Hello, World!")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.10"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═c4779372-7c81-11f0-043a-897064d0eec1
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002

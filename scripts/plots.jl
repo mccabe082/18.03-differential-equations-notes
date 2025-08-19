94021c3eecffe4febdcd621cb6828bb6175d98d4
david.mccabe <david.mccabe@dysismedical.com>
2025-08-19 14:31:43 +0100

    reseat dependancies

--- scripts/plots.jl
+++ scripts/plots.jl
@@ -1,3 +1,14 @@
+
+#>using Pkg
+#>Pkg.activate(".")  # activate the current directory as a project
+#>Pkg.instantiate()  # make sure all packages in Project.toml are installed
 #>julia --project=.
 #>]
 #>add Plots
+
+using Plots
+
+
+f(x) = 10*sin(x)
+
+plot(f, 0, 2π)
